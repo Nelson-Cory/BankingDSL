c8df3c8648fe08970a2a4a121fe4439c43f4b43f
Cory Nelson <nelsonc22@csp.edu>
2026-04-28 01:28:43 -0500

    Update spec_testing.py

---
+++ FinalProject/spec_testing.py
@@ -0,0 +1,113 @@
+from BankAccount import BankAccount
+import unittest
+
+
+RUN_ALL_TESTS = 1
+RUN_EACH_TEST_SEPARATELY = 2
+SHOW_ALL_TESTS = 3
+EXIT = 4
+
+
+def main():
+
+
+    user_input = 0
+
+    while user_input != EXIT:
+
+        user_input = display_test_options()
+
+        if user_input == RUN_ALL_TESTS:
+            print("====Running All Tests====")
+            run_all_tests()
+
+        elif user_input == RUN_EACH_TEST_SEPARATELY:
+            print("====Running Each Test Separately====")
+
+        elif user_input == SHOW_ALL_TESTS:
+            print("")
+
+
+        elif user_input == EXIT:
+            print("====Leaving Testing Environment====")
+
+        else:
+            print("Invalid option")
+
+
+
+def display_test_options():
+        print('\n1. Run all tests')
+        print("2. Run each test separately")
+        print("3. Show all tests")
+        print("4. Exit")
+
+        choice = input("Choose an option: ").upper()
+
+        # Validate menu choice
+        while choice != "EXIT" and (not choice.isdigit() or int(choice) < 1 or int(choice) > 4):
+            print("Invalid selection. Please choose a number (1-4).")
+            choice = input("Choose an option: ").upper()
+
+        result = EXIT
+
+        if choice != "EXIT":
+            result = int(choice)
+
+        return result
+
+def run_all_tests():
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestBankAccount)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+
+class TestBankAccount(unittest.TestCase):
+
+
+    def test_balance_after_withdrawal(self):
+        acct = BankAccount("Cory", "Nelson", "CN856235", 800)
+        actual = acct.withdrawal(50)
+        expected_outcome = 750
+
+        if actual == expected_outcome:
+            print("PASS")
+        else:
+            print("FAIL")
+
+        self.assertEqual(expected_outcome, actual)
+
+
+
+    def test_balance_after_deposit(self):
+        acct = BankAccount("Cory", "Nelson", "CN856235", 800)
+        actual = acct.deposit(50)
+        expected_outcome = 850
+
+        if actual == expected_outcome:
+            print("PASS")
+        else:
+            print("FAIL")
+
+        self.assertEqual(expected_outcome,actual)
+
+
+    def test_account_name_created(self):
+        acct = BankAccount("Cory", "Nelson", "CN856235", 800)
+        actual = acct.get_acct_num()
+        expected_outcome = "CN856235"
+
+
+        if actual == expected_outcome:
+            print("PASS: ")
+        else:
+            print("FAIL: ")
+
+        self.assertEqual(expected_outcome, actual)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
